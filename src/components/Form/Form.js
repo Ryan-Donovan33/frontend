bd776f5098e4da3d3092e3d941a176a91bb8315e
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-29 21:21:06 -0500

    imported formik and setting up on RegForm component

--- src/components/Form/Form.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function RegForm() {
-	return <div>Add registeration form here!</div>;
-}
-
-export default RegForm;
---
+++ src/components/RegForm/RegForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Form, Field, withFormik } from 'formik';
+
+function RegForm() {
+	return (
+		<div>
+			<h3>Let's get started!</h3>
+			<h4>First, let's get your information</h4>
+			<Form>
+				<Field />
+			</Form>
+		</div>
+	);
+}
+
+export default RegForm;
