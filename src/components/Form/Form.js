3fdc6c8d8abb2623c27feed9a19992c3f0a10d03
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-29 21:05:39 -0500

    Added components folder and laying out structure for registeration form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RegForm() {
+	return <div>Add registeration form here!</div>;
+}
+
+export default RegForm;
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+// Import View Here
+
+export default function RouterView() {
+	return (
+		<div className="router-view">
+			<Route>
+				<div>route needed</div>
+			</Route>
+		</div>
+	);
+}
