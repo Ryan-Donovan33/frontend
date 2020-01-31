fe3fdc337d3b11959d3e2c0852cce89f65e63911
brianetaveras <taveras574@gmail.com>
2020-01-30 21:36:37 -0500

    Added navigation component

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Gigapet from '../components/dashboard/Gigapet';
 import HealthCard from '../components/dashboard/HealthCard';
+import Navbar from '../components/layout/Navbar';
 
 
 const Dashboard = ()=>{
     return (
-        <div className="dashboard container">
+        <div className="dashboard">
+            <Navbar/>
+            <div className="container">
             <Gigapet/>
             <HealthCard/>
+
+            </div>
         </div>
     )
 }
