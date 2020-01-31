57c952e5243e434be23545fd0d11f12e0e6cc401
Mysti Dyse <mystidyse@gmail.com>
2020-01-30 17:05:17 -0800

    completed framework for the Header of the login page just missing the logo image. for some reason i couldnt get it to show up on my computer. also added the imports of the login comoponents to the loginPage.js

---
+++ src/components/login/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoginHeader = () => {
+
+    return(
+        <div className='headerLogo'>
+           <img  src='./' />
+        </div>
+        <div>
+            <h2>Welcome to GigaPet!</h2>
+        </div>
+    )
+}
+export default LoginHeader;
