9bb0408ca0393fc0d9506f88bdf20dfbbeb8d655
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-30 20:50:14 -0500

    Adding first field to Child Info component form

--- src/components/ChildInfo/ChildInfo.js
+++ src/components/ChildInfo/ChildInfo.js
@@ -3,8 +3,26 @@ import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import Axios from 'axios';
 
-function ChildInfo() {
-	return <div />;
-}
+const ChildInfo = ({ errors, touched, values, status }) => {
+	const [ child, setChild ] = useState([]);
+
+	useEffect(
+		() => {
+			status && setChild((person) => [ ...person, status ]);
+		},
+		[ status ]
+	);
+
+	return (
+		<div>
+			<h3>Great! You are almost done.</h3>
+			<h4>Let's get your child's information below!</h4>
+			<Form>
+				<Field type="text" name="name" placeholder="Name" value={values.name} />
+				{touched.name && errors.name && <p>{errors.name}</p>}
+			</Form>
+		</div>
+	);
+};
 
 export default ChildInfo;
