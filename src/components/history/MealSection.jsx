b1e24eb0ef7abb6d41d749e72f75028df4d5e18d
brianetaveras <taveras574@gmail.com>
2020-01-31 17:33:26 -0500

    Connected Add food flow with router

--- src/components/history/MealSection.jsx
+++ src/components/history/MealSection.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
+export default function MealSection({ title, meals, ...props }) {
+  const SectionTitle = styled.div`
+    font-weight: bold;
+    border-bottom: 1px solid #f1f1f1;
+    margin-bottom: 5px;
+  `;
+  const MealTitle = styled.div`
+    margin: 10px 0;
+    cursor: pointer;
+    background: #f2f4ff;
+    padding: 20px 10px;
+    border-radius: 10px;
+  `;
 
-export default function MealSection({title, meals}){
-    const SectionTitle = styled.div`
-        font-weight: bold;
-        border-bottom: 1px solid #f1f1f1; 
-        margin-bottom: 5px;
-    `
-    const MealTitle = styled.div`
-        margin: 10px 0;
-        cursor: pointer;
-        background: #F2F4FF;
-        padding: 20px 10px;
-        border-radius: 10px;
+  const AddButton = styled.div`
+    text-transform: uppercase;
+    text-align: center;
+    width: 100%;
+    font-weight: bold;
+    color: #4864e6;
+    cursor: pointer;
+  `;
 
-    `
-
-    const AddButton = styled.div`
-    
-        text-transform: uppercase;
-        text-align: center;
-        width: 100%;
-        font-weight: bold;
-        color: #4864E6;
-        cursor: pointer;
-
-    
-    `
-
-    return (
-        <div className="meal-section">
-            <SectionTitle>{title}</SectionTitle>
-            {meals.map(meal=>{
-                return (<MealTitle key={meal.title}>{meal.title}</MealTitle>)
-            })}
-            <AddButton>Add Food</AddButton>
-
-        </div>
-    );
-};
+  return (
+    <div className="meal-section">
+      <SectionTitle>{title}</SectionTitle>
+      {meals.map(meal => {
+        return <MealTitle key={meal.title}>{meal.title}</MealTitle>;
+      })}
+      <AddButton
+        onClick={e => {
+          e.preventDefault();
+          props.history.push("add");
+        }}
+      >
+        Add Food
+      </AddButton>
+    </div>
+  );
+}
