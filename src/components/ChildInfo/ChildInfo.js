ba6e164098fb6af10dca5dd97db90df12c16e410
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-04 20:00:58 -0500

    Workingn on styling of childinfo

--- src/components/ChildInfo/ChildInfo.js
+++ src/components/ChildInfo/ChildInfo.js
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
 import styled from 'styled-components';
-import { OnboardingButton, InputStyle } from '../GeneralStyling';
+import { OnboardingButton, InputStyle, IconStyle } from '../GeneralStyling';
 
 const FormInput = styled.input`
 	width: 100%;
@@ -33,28 +33,34 @@ const ChildCard = ({ errors, touched, values, status }) => {
 				<h4>Let's get your child's information below!</h4>
 			</div>
 			<Form>
-				<FormInput type="text" name="name" placeholder="Name" value={values.name} />
+				<Field style={InputStyle} type="text" name="name" placeholder="Name" value={values.name} />
 				{touched.name && errors.name && <p>{errors.name}</p>}
 
-				<label style={{ color: 'white' }}>
+				<label style={IconStyle}>
 					<img src="assets/Icon ionic-md-male.svg" alt="Male Icon" />
 					<Field type="checkbox" name="male" placeholder="Male" value={values.male} />
 					Male
 				</label>
 
-				<label style={{ color: 'white' }}>
+				<label style={IconStyle}>
 					<img src="assets/Icon ionic-ios-arrow-non-binary.svg" alt="Non-Binary Icon" />
 					<Field type="checkbox" name="tbd" value={values.tbd} />
 					Non-Binary
 				</label>
 
-				<label style={{ color: 'white' }}>
+				<label style={IconStyle}>
 					<img src="assets/Icon ionic-md-female.svg" alt="Female Icon" />
 					<Field type="checkbox" name="female" placeholder="Female" value={values.female} />
 					Female
 				</label>
 
-				<FormInput type="date" name="date" placeholder="Date of Birth (mm/dd/yyyy)" value={values.date} />
+				<Field
+					style={InputStyle}
+					type="date"
+					name="date"
+					placeholder="Date of Birth (mm/dd/yyyy)"
+					value={values.date}
+				/>
 
 				<Link to="">
 					<OnboardingButton>Wrap Up!</OnboardingButton>
@@ -73,28 +79,29 @@ const ChildInfo = withFormik({
 			female: false,
 			date: ''
 		};
+	},
+
+	validationSchema: Yup.object().shape({
+		name: Yup.string().required('Please fill out name'),
+		date: Yup.string().required('Please fill out date'),
+		tbd: Yup.bool(),
+		male: Yup.bool(),
+		female: Yup.bool()
+	}),
+
+	handleSubmit(values, { setStatus, resetForm }) {
+		console.log('submitting form:', values);
+
+		Axios.post('', values)
+			.then((res) => {
+				console.log('Success:', res);
+				setStatus(res.data);
+				resetForm();
+			})
+			.catch((err) => {
+				console.log('Error:', err.response);
+			});
 	}
-
-	// validationSchema: Yup.object().shape({
-	// 	name: Yup.string().required('Please fill out name'),
-	// 	date: Yup.string().required('Please fill out date'),
-	// 	tbd: Yup.bool(),
-	// 	male: Yup.bool(),
-	// 	female: Yup.bool()
-	// })
-	// handleSubmit(values, { setStatus, resetForm }) {
-	// 	console.log('submitting form:', values);
-
-	// 	Axios.post('https://gigapetdb.herokuapp.com/', values)
-	// 		.then((res) => {
-	// 			console.log('Success:', res);
-	// 			setStatus(res.data);
-	// 			resetForm();
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log('Error:', err.response);
-	// 		});
-	// }
 })(ChildCard);
 
 export default ChildInfo;
--- src/components/RegForm/RegForm.js
+++ src/components/RegForm/RegForm.js
@@ -1,20 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
-import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
 import Axios from 'axios';
 import { OnboardingButton, InputStyle } from '../GeneralStyling';
-import styled from 'styled-components';
-
-const FormInput = styled.input`
-	width: 100%;
-	height: 50px;
-	border: 2px solid #4864e6;
-	border-radius: 15px;
-	padding: 0 5px;
-	box-sizing: border-box;
-	font-size: 16px;
-`;
+import { Link } from 'react-router-dom';
 
 const Register = ({ errors, touched, values, status, ...props }) => {
 	const [ user, setUser ] = useState([]);
@@ -32,16 +21,28 @@ const Register = ({ errors, touched, values, status, ...props }) => {
 				<h4>First, let's get your information</h4>
 			</div>
 			<Form>
-				<FormInput type="text" name="name" placeholder="Name" value={values.name} />
+				<Field style={InputStyle} type="text" name="name" placeholder="Name" value={values.name} />
 				{touched.name && errors.name && <p>{errors.name}</p>}
 
-				<FormInput type="email" name="email" placeholder="Email" value={values.email} />
+				<Field style={InputStyle} type="email" name="email" placeholder="Email" value={values.email} />
 				{touched.email && errors.email && <p>{errors.email}</p>}
 
-				<FormInput type="password" name="password" placeholder="Password" value={values.password} />
+				<Field
+					style={InputStyle}
+					type="password"
+					name="password"
+					placeholder="Password"
+					value={values.password}
+				/>
 				{touched.password && errors.password && <p>{errors.password}</p>}
 
-				<FormInput type="password" name="confirm" placeholder="Confirm Password" value={values.confirm} />
+				<Field
+					style={InputStyle}
+					type="password"
+					name="confirm"
+					placeholder="Confirm Password"
+					value={values.confirm}
+				/>
 				{touched.confirm && errors.confirm && <p>{errors.confirm}</p>}
 				<Link to="/childinfo">
 					<OnboardingButton type="submit">Next</OnboardingButton>
@@ -60,29 +61,26 @@ export default withFormik({
 			password: '',
 			confirm: ''
 		};
-	}
+	},
 
 	//validation required - making sure all users fill out each field.
-	// validationSchema: Yup.object().shape({
-	// 	name: Yup.string().required('Please fill in your name!'),
-	// 	email: Yup.string().required('Please provide your email!'),
-	// 	password: Yup.string().required('Password Required!'),
-	// 	confirm: Yup.string().required('Confirm Password!')
-	// })
+	validationSchema: Yup.object().shape({
+		name: Yup.string().required('Please fill in your name!'),
+		email: Yup.string().required('Please provide your email!'),
+		password: Yup.string().required('Password Required!'),
+		confirm: Yup.string().required('Confirm Password!')
+	}),
+	handleSumbit(values, { setStatus, resetForm }) {
+		console.log('submitting form:', values);
 
-	// Don't think I need axios for this but keeping just incase I will need it once we involve the backend
-
-	// handleSumbit(values, { setStatus, resetForm }) {
-	// 	console.log('submitting form:', values);
-
-	// 	Axios.post('https://gigapetdb.herokuapp.com/', values)
-	// 		.then((res) => {
-	// 			console.log(res, 'successful');
-	// 			setStatus(res);
-	// 			resetForm();
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log('Error', err);
-	// 		});
-	// }
+		Axios.post('', values)
+			.then((res) => {
+				console.log(res, 'successful');
+				setStatus(res);
+				resetForm();
+			})
+			.catch((err) => {
+				console.log('Error', err);
+			});
+	}
 })(Register);
