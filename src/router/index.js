282822cda86f402eace5c966d9e77e0a70affb92
brianetaveras <taveras574@gmail.com>
2020-02-06 18:44:55 -0500

    Added router guard to all the routes

--- src/router/index.js
+++ src/router/index.js
@@ -16,10 +16,10 @@ import UpdateEntry from '../views/UpdateEntry';
 export default function RouterView() {
 	return (
 		<div className="router-view">
-			<Route exact path="/" component={Dashboard} />
-			<Route exact path="/history" component={History} />
-			<Route exact path="/add" component={AddEntry} />
-			<Route path="/update/:id" component={UpdateEntry} />
+			<RouteGuard exact path="/" component={Dashboard} />
+			<RouteGuard exact path="/history" component={History} />
+			<RouteGuard exact path="/add" component={AddEntry} />
+			<RouteGuard path="/update/:id" component={UpdateEntry} />
 			<Route path="/login" component={LoginPage} />
 			<Route path="/onboarding-1" component={RegPage} />
 			<Route path="/childinfo" component={ChildPage} />
